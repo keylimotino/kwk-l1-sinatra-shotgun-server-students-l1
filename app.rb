0b4b99bd1bd20597220d28e5124f4a5909d672ff
keylimotino <stephanie48758@gmail.com>
2018-06-12 15:25:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! keyli "
+  get '/' do
+    "Welcome to your app!!!! keyli is hungry"
   end
 
 end
